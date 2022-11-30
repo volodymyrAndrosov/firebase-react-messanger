4a5a590a39c3211cacc421dded3e921e5c880e83
volodymyrAndrosov <1999vovan@ukr.net>
2022-12-01 00:16:15 +0200

    add styling for LoginPage

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
 
-export default LoginPage
+  return (
+    <Container disableGutters >
+      <Grid container
+        style={{ height: window.innerHeight - 50, }}
+        alignItems='center'
+        justifyContent='center'
+      >
+        <Grid style={{ width: '100%', background: 'lightgray', height: '100%' }}
+          container
+          alignItems='center'
+          justifyContent='center'
+          direction='column'
+        >
+          <Box p={5}>
+            <Button variant='outlined'>Войти с помощью Google</Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container >
+  );
+};
+
+export default LoginPage;
