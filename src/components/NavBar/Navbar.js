917f0afcf6d055c181d59f1d6bc0293f2cf51579
volodymyrAndrosov <1999vovan@ukr.net>
2022-12-01 00:16:28 +0200

    add styling for NavBar component

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../../utils/consts';
 
 const Navbar = () => {
+    const isUser = true;
+
     return (
-        <div>Navbar</div>
-    )
-}
+        <AppBar position="static">
+            <Toolbar variant={"dense"}>
+                <Grid container justifyContent="flex-end">
+                    {isUser ?
+                        <Button variant="contained">Log out</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button>Log in</Button>
+                        </NavLink>
+                    }
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default Navbar;
