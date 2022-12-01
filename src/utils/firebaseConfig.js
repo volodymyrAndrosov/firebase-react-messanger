425987be9fa99dfb060512bac4776448c80011b2
volodymyrAndrosov <1999vovan@ukr.net>
2022-12-01 16:17:22 +0200

    create and separate config firebase

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCltosyLwogA_jFgm81DwaRTW_9NaS--RQ",
+    authDomain: "fir-react-messanger-f5f10.firebaseapp.com",
+    projectId: "fir-react-messanger-f5f10",
+    storageBucket: "fir-react-messanger-f5f10.appspot.com",
+    messagingSenderId: "1030203397454",
+    appId: "1:1030203397454:web:546b336ca9e4a635523a1f",
+    measurementId: "G-6D7FJDQGL5"
+};
